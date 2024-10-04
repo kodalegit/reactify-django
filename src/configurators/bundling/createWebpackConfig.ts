30ff1ca9ec44d13462242ab62503b92f49ff0569
kodalegit <victorkimani77@gmail.com>
2024-10-05 02:00:09 +0300

    add clean option in webpack

--- src/configurators/bundling/createWebpackConfig.ts
+++ src/configurators/bundling/createWebpackConfig.ts
@@ -18,6 +18,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'static/${appName}/js'),
         filename: 'bundle.js',
+        clean: true
     },
     module: {
         rules: [
