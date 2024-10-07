ecf6b7003b5ade2b0807a81bd619be162cca9b40
kodalegit <victorkimani77@gmail.com>
2024-10-07 20:59:07 +0300

    fix bundling logging

--- src/configurators/bundling/configureBundling.ts
+++ src/configurators/bundling/configureBundling.ts
@@ -1,3 +1,4 @@
+import { highlighter } from "@/src/utils/highlighter";
 import { createBabelConfig } from "./createBabelConfig";
 import { createWebpackConfig } from "./createWebpackConfig";
 import { updatePackageJsonScripts } from "./updatePackageJsonScripts";
@@ -12,6 +13,12 @@ export async function configureBundling(
     await createWebpackConfig(appName, useTypescript, appPath);
     await createBabelConfig(useTypescript, appPath);
     await updatePackageJsonScripts(appPath);
+    logger.break();
+    logger.success(
+      `${highlighter.info(`Webpack`)} and ${highlighter.info(
+        "Babel"
+      )} successfully configured.`
+    );
   } catch (error) {
     logger.break();
     logger.error(`Error configuring bundling: ${(error as Error).message}`);
