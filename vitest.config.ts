09cdfeff9ba438eaf7a52c6752fadc804359b80b
kodalegit <victorkimani77@gmail.com>
2024-10-10 00:08:13 +0300

    add test coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
     globals: true, // Enable global testing functions (describe, it, etc.)
-    environment: 'node', // Set the test environment
+    environment: "node",
+    coverage: {
+      provider: "istanbul",
+    },
   },
 });
